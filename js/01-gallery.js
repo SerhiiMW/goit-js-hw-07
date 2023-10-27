e57e7a8579af495f95a8e9b0d0622b71f6dd2201
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2023-10-27 12:05:07 +0300

    gallery1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const list = document.querySelector(".gallery")
+
+list.insertAdjacentHTML("beforeend", creatMarkup(galleryItems))
+list.addEventListener("click", handleclick)
+
+function handleclick(event) {
+    event.preventDefault()
+    if (event.target === event.currentTarget) {
+        return;
+    }
+    const currentImage = event.target.closest(".gallery__image")
+    const currentImageSourse = currentImage.alt
+    const image = galleryItems.find(({ description }) => description === currentImageSourse)
+ 
+    const instance = basicLightbox.create(`
+    <img src="${image.original}" width="800" height="600">
+`)
+
+instance.show()
+}
+
+function creatMarkup(arr) {
+    return arr.map(({preview, original, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image"
+    src="${preview}" 
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+    </li>
+    `).join("")
+}
