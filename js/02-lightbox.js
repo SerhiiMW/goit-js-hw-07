9f927e1d767b7905e335523a9f29d8f7840bfc7d
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2023-10-27 14:33:08 +0300

    lightbox2.1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,20 +4,7 @@ import { galleryItems } from "./gallery-items.js";
 const list = document.querySelector(".gallery");
 
 list.insertAdjacentHTML("beforeend", creatMarkup(galleryItems));
-list.addEventListener("click", handleclick);
 
-function handleclick(event) {
-  event.preventDefault();
-  if (event.target === event.currentTarget) {
-    return;
-  }
-  const currentImage = event.target.closest(".gallery__image");
-  const currentImageSourse = currentImage.alt;
-  const image = galleryItems.find(
-    ({ description }) => description === currentImageSourse
-  );
-
-    
     var lightbox = new SimpleLightbox(".gallery a", {
         nav: true,
         enableKeyboard: true,
@@ -25,8 +12,6 @@ function handleclick(event) {
         captionPosition: 'bottom',
         captionDelay: 250,
     });
-
-}
 
 function creatMarkup(arr) {
   return arr.map(({preview, original, description }) => `
